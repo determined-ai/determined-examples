788b170c2ed5e8c2c9e30bdecad1cc188ddc51bb
KevinMusgrave <takeshi.musgrave@hpe.com>
2024-02-12 09:11:01 -0500

    Get rid of max_length function

--- blog/llm-finetuning/scrap.py
+++ blog/llm-finetuning/scrap.py
@@ -1,13 +1,10 @@
-from finetune import get_model_and_tokenizer
-from dataset_utils import load_or_create_dataset
-from chat_format import (
-    get_chat_format,
-    get_response_template_ids,
-    get_tokenizer_max_length,
-)
 import tqdm
 
-model_name = "mistralai/Mistral-7B-Instruct-v0.2"
+from chat_format import get_chat_format, get_response_template_ids
+from dataset_utils import load_or_create_dataset
+from finetune import get_model_and_tokenizer
+
+model_name = "TinyLlama/TinyLlama-1.1B-Chat-v0.4"
 model, tokenizer = get_model_and_tokenizer(model_name)
 
 
@@ -17,7 +14,6 @@ def validate():
 
     for difficulty in ["easy", "medium", "hard"]:
         dataset = load_or_create_dataset(difficulty)
-        max_length = get_tokenizer_max_length(difficulty, model_name)
         for split in ["train", "valid", "test"]:
             print(difficulty, split)
             for x in tqdm.tqdm(dataset[split]):
@@ -25,7 +21,9 @@ def validate():
                     get_chat_format(x, model_name), tokenize=False
                 )
                 element = tokenizer(
-                    formatted, padding=True, truncation=True, max_length=max_length
+                    formatted,
+                    padding="longest",
+                    truncation=True,
                 )["input_ids"]
                 response_template = to_str(
                     get_response_template_ids(tokenizer, model_name)
@@ -40,20 +38,4 @@ def validate():
                 #     raise ValueError("decoded != formatted")
 
 
-def get_max_length():
-    for difficulty in ["easy", "medium", "hard"]:
-        max_length = 0
-        dataset = load_or_create_dataset(difficulty)
-        for split in ["train", "valid", "test"]:
-            print(difficulty, split)
-            for element in tqdm.tqdm(dataset[split]):
-                element = tokenizer.apply_chat_template(
-                    get_chat_format(element, model_name), tokenize=False
-                )
-                element = tokenizer(element, return_length=True)
-                max_length = max(max_length, element["length"][0])
-        print("max_length", max_length)
-
-
-# get_max_length()
 validate()
