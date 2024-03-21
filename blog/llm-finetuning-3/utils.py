08bfd14175a9e81234c57c17d147bfaf3f84627f
aciborowska <aga.ciborowska@gmail.com>
2024-03-20 18:39:44 -0700

    fix model download from ckpt

--- blog/llm-finetuning-3/utils.py
+++ blog/llm-finetuning-3/utils.py
@@ -2,7 +2,6 @@ from typing import Optional
 import os
 import determined as det
 import torch
-from peft import AutoPeftModelForCausalLM
 from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedModel
 from transformers.trainer_utils import get_last_checkpoint
 
@@ -11,7 +10,7 @@ from chat_format import CHAT_ML_TEMPLATE
 
 def get_model(model_name, inference=False, device_map="auto") -> PreTrainedModel:
     if inference:
-        model = AutoPeftModelForCausalLM.from_pretrained(
+        model = AutoModelForCausalLM.from_pretrained(
             model_name, torch_dtype=torch.bfloat16, device_map=device_map
         )
     else:
@@ -48,29 +47,36 @@ def get_tokenizer(
 def download_ckpt(ckpt_uuid: str, core_context: det.core.Context) -> str:
     download_dir = os.path.join(os.environ.get("HF_CACHE", "."), ckpt_uuid)
 
-    if not os.path.exists(os.path.join(download_dir, ckpt_uuid)):
-        os.makedirs(download_dir)
-
-        def selector(path: str) -> bool:
-            if any(
-                [
-                    path.endswith(ext)
-                    for ext in [
-                        "config.json",
-                        "generation-config.json",
-                        ".safetensors",
-                        "special_tokens_map.json",
-                        "tokenizer_config.json",
-                        "tokenizer.json",
-                        "tokenizer.model",
-                    ]
+    def selector(path: str) -> bool:
+        if any(
+            [
+                path.endswith(ext)
+                for ext in [
+                    "config.json",
+                    "generation-config.json",
+                    ".safetensors",
+                    "special_tokens_map.json",
+                    "tokenizer_config.json",
+                    "tokenizer.json",
+                    "tokenizer.model",
+                    "model.safetensors.index.json",
                 ]
-            ):
-                return True
+            ]
+        ):
+            return True
 
-            return False
+        return False
 
-        core_context.checkpoint.download(ckpt_uuid, download_dir, selector=selector)
+    # ['trainer_state.json', 'global_step5000', 'config.json', 'rng_state_1.pth', 'training_args.bin', 'latest',
+    #  'rng_state_5.pth', 'rng_state_4.pth', 'model-00002-of-00002.safetensors', 'tokenizer_config.json',
+    #  'rng_state_2.pth', 'rng_state_3.pth', 'model-00001-of-00002.safetensors', 'model.safetensors.index.json',
+    #  'special_tokens_map.json', 'zero_to_fp32.py', 'rng_state_6.pth', 'rng_state_7.pth', 'tokenizer.json',
+    #  'generation_config.json', 'rng_state_0.pth']
 
+    core_context.checkpoint.download(ckpt_uuid, download_dir, selector=selector)
+
+    import logging
     model_dir = get_last_checkpoint(download_dir)
+
+    logging.error(os.listdir(model_dir))
     return model_dir
