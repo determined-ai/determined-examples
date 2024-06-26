a65842cec828394a5b5f23ce7c65bacaa2ba7f6a
Garrett Goon <garrett.goon@hpe.com>
2024-06-26 15:27:08 -0400

    layers

--- blog/tp/layers.py
+++ blog/tp/layers.py
@@ -1,155 +1,136 @@
-from typing import Optional, Union
+from typing import Any, Optional, Union
 
-import einops
 import torch
+import torch.distributed as dist
 import torch.nn as nn
-import torch.nn.functional as F
-
-
-class Attention(nn.Module):
-    """
-    Minimal multi-head attention layer.
-    """
-
-    def __init__(
-        self,
-        d_model: int,
-        n_heads: int,
-        device: Optional[Union[str, torch.device]] = None,
-        dtype: Optional[torch.dtype] = None,
-    ):
-        super().__init__()
-        self.d_model = d_model
-        self.n_heads = n_heads
-        factory_kwargs = {"device": device, "dtype": dtype}
-
-        self.d_head, remainder = divmod(self.d_model, self.n_heads)
-        assert not remainder, f"{n_heads=} must divide {d_model=} evenly"
-
-        self.lin_qkv = nn.Linear(
-            self.d_model,
-            3 * self.d_model,
-            **factory_kwargs,
-        )
-
-        self.lin_out = nn.Linear(self.d_model, self.d_model, **factory_kwargs)
-
-    def forward(
-        self,
-        inputs: torch.Tensor,
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        bsz, seq_len, _ = inputs.size()
-
-        # Create the queries, keys, values
-        qkv = einops.rearrange(
-            self.lin_qkv(inputs),
-            "b s (three n_h d_h) -> three b s n_h d_h",
-            b=bsz,
-            s=seq_len,
-            three=3,
-            n_h=self.n_heads,
-            d_h=self.d_head,
-        )
-        q, k, v = qkv
-
-        bsz, seq_len, n_heads, d_head = q.shape
-
-        shape_kwargs = dict(b=bsz, n_h=n_heads, s=seq_len, d_h=d_head)
-        q = einops.rearrange(q, "b s n_h d_h -> b n_h s d_h", **shape_kwargs)
-        k = einops.rearrange(k, "b s n_h d_h -> b n_h s d_h", **shape_kwargs)
-        v = einops.rearrange(v, "b s n_h d_h -> b n_h s d_h", **shape_kwargs)
-
-        # Multi-head self-attention
-        attn_output = F.scaled_dot_product_attention(q, k, v, is_causal=True)
-        attn_output = einops.rearrange(
-            attn_output,
-            "b n_h s d_h -> b s (n_h d_h)",
-            b=bsz,
-            n_h=n_heads,
-            s=seq_len,
-            d_h=d_head,
-        )
-
-        # Final projection
-        out = self.lin_out(attn_output)
-
-        return out
 
 
 class MLP(nn.Module):
     """
-    Basic MLP layer with optional Dropout.
+    Basic MLP (multi-layer perceptron) layer. Dropout is neglected.
     """
 
     def __init__(
         self,
         d_model: int,
-        act_fn: nn.Module,
-        dropout_prob: Optional[float] = None,
         device: Optional[Union[str, torch.device]] = None,
         dtype: Optional[torch.dtype] = None,
     ) -> None:
         super().__init__()
-        self.d_model = d_model
-        self.act_fn = act_fn
-        self.dropout_prob = dropout_prob
-        factory_kwargs = {"device": device, "dtype": dtype}
 
-        self.lin_0 = nn.Linear(self.d_model, 4 * self.d_model, **factory_kwargs)
-        self.lin_1 = nn.Linear(4 * self.d_model, self.d_model, **factory_kwargs)
-        self.dropout = nn.Dropout(self.dropout_prob) if self.dropout_prob else None
+        self.lin_0 = nn.Linear(d_model, 4 * d_model, device=device, dtype=dtype)
+        self.act_fn = nn.GELU()
+        self.lin_1 = nn.Linear(4 * d_model, d_model, device=device, dtype=dtype)
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         x = self.lin_0(inputs)
         x = self.act_fn(x)
         x = self.lin_1(x)
-        if self.dropout is not None:
-            x = self.dropout(x)
         return x
 
 
-class Block(nn.Module):
-    """
-    Basic transformer block.
+class AllReduceFwdIdentityBwd(torch.autograd.Function):
+    @staticmethod
+    def forward(
+        ctx: Any, inputs: torch.Tensor, group: Optional[dist.ProcessGroup] = None
+    ) -> torch.Tensor:
+        inputs = inputs.clone()
+        dist.all_reduce(inputs, group=group)
+        return inputs
 
-    Schematic:
-    ┌──────┐
-    │inputs│
-    └┬─┬───┘
-     │┌▽───────────┐
-     ││norm_0, attn│
-     │└┬───────────┘
-    ┌▽─▽──┐
-    │ add │
-    └┬─┬──┘
-     │┌▽──────────┐
-     ││norm_1, mlp│
-     │└┬──────────┘
-    ┌▽─▽──┐
-    │ add │
-    └┬────┘
-    ┌▽──────┐
-    │outputs│
-    └───────┘
+    @staticmethod
+    def backward(ctx: Any, grad_outputs: torch.Tensor) -> tuple[torch.Tensor, None]:
+        return grad_outputs, None
+
+
+class IdentityFwdAllReduceBwd(torch.autograd.Function):
+    @staticmethod
+    def forward(
+        ctx: Any, inputs: torch.Tensor, group: Optional[dist.ProcessGroup] = None
+    ) -> torch.Tensor:
+        ctx.group = group
+        return inputs
+
+    @staticmethod
+    def backward(ctx: Any, grad_outputs: torch.Tensor) -> tuple[torch.Tensor, None]:
+        grad_outputs = grad_outputs.clone()
+        dist.all_reduce(grad_outputs, group=ctx.group)
+        return grad_outputs, None
+
+
+class LinearShardedOutputs(nn.Linear):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        group: dist.ProcessGroup,
+        device: Optional[Union[str, torch.device]] = None,
+        dtype: Optional[torch.dtype] = None,
+    ) -> None:
+        sharded_out_features, remainder = divmod(out_features, group.size())
+        assert not remainder, "out_features must be divisible by tp_degree"
+        super().__init__(
+            in_features=in_features,
+            out_features=sharded_out_features,
+            device=device,
+            dtype=dtype,
+        )
+
+        self.group = group
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        x = IdentityFwdAllReduceBwd.apply(inputs, self.group)
+        x = super().forward(x)
+        return x
+
+
+class LinearShardedInputs(nn.Linear):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        group: dist.ProcessGroup,
+        device: Optional[Union[str, torch.device]] = None,
+        dtype: Optional[torch.dtype] = None,
+    ) -> None:
+        sharded_in_features, remainder = divmod(in_features, group.size())
+        assert not remainder, "out_features must be divisible by tp_degree"
+        super().__init__(
+            in_features=sharded_in_features,
+            out_features=out_features,
+            device=device,
+            dtype=dtype,
+        )
+        self.group = group
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        x = inputs @ self.weight.T
+        # Crucial: all-reduce/reduce-scatter _before_ adding the bias.
+        x = AllReduceFwdIdentityBwd.apply(x, self.group)
+        x = x + self.bias
+        return x
+
+
+class MLPTP(MLP):
+    """
+    Basic Tensor Parallel MLP (multi-layer perceptron) layer. Dropout is neglected.
     """
 
     def __init__(
         self,
         d_model: int,
-        n_heads: int,
-        act_fn: nn.Module,
-        dropout_prob: Optional[float] = None,
-        dtype: Optional[torch.dtype] = None,
+        group: Optional[dist.ProcessGroup] = None,
         device: Optional[Union[str, torch.device]] = None,
-    ):
-        super().__init__()
-        factory_kwargs = {"device": device, "dtype": dtype}
-        self.attn = Attention(d_model=d_model, n_heads=n_heads, **factory_kwargs)
-        self.mlp = MLP(d_model=d_model, act_fn=act_fn, dropout_prob=dropout_prob, **factory_kwargs)
-        self.norm_0 = nn.LayerNorm(d_model, **factory_kwargs)
-        self.norm_1 = nn.LayerNorm(d_model, **factory_kwargs)
+        dtype: Optional[torch.dtype] = None,
+    ) -> None:
+        nn.Module.__init__(self)
+        # Fallback to the WORLD process group, if None provided
+        group = group or dist.group.WORLD
 
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        outputs = self.attn(self.norm_0(inputs)) + inputs
-        outputs = self.mlp(self.norm_1(outputs)) + outputs
-        return outputs
+        self.lin_0 = LinearShardedOutputs(
+            d_model, 4 * d_model, group=group, device=device, dtype=dtype
+        )
+        self.act_fn = nn.GELU()
+        self.lin_1 = LinearShardedInputs(
+            4 * d_model, d_model, group=group, device=device, dtype=dtype
+        )
